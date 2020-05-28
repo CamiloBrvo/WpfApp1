e87d7c0743d2e5e9d5f2f3eeacad429c29a27aea
Camilo BRAVO <61042366+CamiloBrvo@users.noreply.github.com>
2020-05-28 12:03:24 +0200

    Add files via upload

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -16,9 +17,6 @@ using System.Data.SqlClient;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -28,62 +26,125 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)//btnConnexion
         {
+            fonctionLogin();
+        }
+
+       
+        
+
+        private void TextBox_MDP_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine("GUI identifiant(mail)" + this.txbIdentifiant_GUI.Text);
+            Console.WriteLine("GUI pwd(pwd)" + this.txbMotDePasse_GUI.Text);
+        }
+
+        private void TxbMotDePasse_GUI_TextInput(object sender, TextCompositionEventArgs e)
+        {
             
+        }
+
+        private void TextBoxIdentifiant_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        /*
+         *  ublic char PasswordChar{ get; set; }
+
+        public void createPasswordTextBox()
+        {
+            txbMotDePasse_GUI.Text = "";
+            txbMotDePasse_GUI.PasswordChar = "*";
+        }
+        */
+        private void fonctionLogin()
+        {
+
+            Console.WriteLine("GUI identifiant(mail) quand bouton click :" + this.txbIdentifiant_GUI.Text);
+            Console.WriteLine("GUI pwd(pwd) quand bouton click :" + this.txbMotDePasse_GUI.Text);
+
+            Console.WriteLine("InitConnection");
             try
             {
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
                 builder.DataSource = "localhost";
-                builder.UserID = "root";
-                builder.Password = "root";
+                builder.IntegratedSecurity = true;
                 builder.InitialCatalog = "stage";
+                Console.WriteLine("Connexion info created : " + builder.ConnectionString);
 
-                Console.WriteLine("OK Connexion");
-                /*
                 using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
                 {
-                    Console.WriteLine("\nQuery data example:");
-                    Console.WriteLine("=========================================\n");
-
                     StringBuilder sb = new StringBuilder();
-                    sb.Append("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName ");
-                    sb.Append("FROM [SalesLT].[ProductCategory] pc ");
-                    sb.Append("JOIN [SalesLT].[Product] p ");
-                    sb.Append("ON pc.productcategoryid = p.productcategoryid;");
-                    String sql = sb.ToString();
+                    sb.Append("SELECT *  FROM responsables WHERE email = '" + this.txbIdentifiant_GUI.Text + "'");
 
+                    int id_responsables = 0;
+                    string mail = "mail";
+
+                    String sql = sb.ToString();
                     using (SqlCommand command = new SqlCommand(sql, connection))
                     {
+                        Console.WriteLine("\nQuery :" + sql + " should be executed");
                         connection.Open();
+                        Console.WriteLine("Connexion opened to database");
+
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
+                            Console.WriteLine("\nExecute Query :" + sql);
                             while (reader.Read())
                             {
-                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                                id_responsables = reader.GetInt32(0);
+                                mail = reader.GetString(6);
+
+                                Console.WriteLine("Le mail correct est : {0}", mail);
                             }
                         }
+
+                        connection.Close();
+                        Console.WriteLine("Connexion closed to database");
                     }
-                }*/
+                    if (mail != "mail")
+                    {
+                        MessageBox.Show("Le mail est correct");
+                        StringBuilder sb2 = new StringBuilder();
+                        sb2.Append("SELECT mdp FROM utilisateur WHERE id_responsables = '" + id_responsables + "'");
+
+                        String sql2 = sb2.ToString();
+                        using (SqlCommand command = new SqlCommand(sql2, connection))
+                        {
+                            Console.WriteLine("\nQuery :" + sql2 + " should be executed");
+                            connection.Open();
+                            Console.WriteLine("Connexion opened to database");
+                            using (SqlDataReader reader2 = command.ExecuteReader())
+                            {
+                                Console.WriteLine("\nExecute Query :" + sql2);
+                                while (reader2.Read())
+                                {
+                                    Console.WriteLine("Le mot de passe correct est : {0}", reader2.GetString(0));
+                                    if (txbMotDePasse_GUI.Text == reader2.GetString(0))
+                                    {
+                                        MessageBox.Show("Le mdp est correct");
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Le mdp est faux");
+                                    }
+                                }
+                            }
+                            connection.Close();
+                            Console.WriteLine("Connexion closed to database");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Le mail n'est pas correct");
+                    }
+                }
             }
             catch (SqlException E)
             {
                 Console.WriteLine(E.ToString());
                 Console.WriteLine("ERROR Connexion");
             }
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)// txbIdentifiant
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)// txbMotDePasse
-        {
-
-        }
-
-        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
-        {
-            
         }
     }
 }
